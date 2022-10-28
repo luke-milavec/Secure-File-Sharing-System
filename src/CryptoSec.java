4cddb4fa72fd0d97731b2d573fa1167f039df0e0
tahmad4 <taha.ahmad77@gmail.com>
2022-10-28 01:01:24 -0400

    Added CryptoSec class helper class; genRSAKeyPair() added

---
+++ src/CryptoSec.java
@@ -0,0 +1,35 @@
+import org.bouncycastle.util.io.pem.PemObject;
+import org.bouncycastle.util.io.pem.PemReader;
+import org.bouncycastle.util.io.pem.PemWriter;
+
+import java.io.*;
+
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+public class CryptoSec {
+    CryptoSec() {
+
+    }
+    public KeyPair genRSAKeyPair() {
+        try {
+            KeyPairGenerator gen = KeyPairGenerator.getInstance("RSA");
+            gen.initialize(2048);
+            return gen.generateKeyPair();
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("There was error generating RSA keypair");
+        }
+        return null;
+    }
+
+
+
+
+}
